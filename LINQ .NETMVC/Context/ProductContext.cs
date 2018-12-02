2d2b00a5190e105794066a0e3e92a80df1bb9652
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-12-03 01:27:46 +0700

    Okie

---
+++ LINQ .NETMVC/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LINQ.NETMVC.Models;
+
+namespace LINQ.NETMVC.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ LINQ .NETMVC/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LINQ.NETMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EntryDate { get; set; }
+
+    }
+}
